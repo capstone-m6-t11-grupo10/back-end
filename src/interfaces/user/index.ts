f9949d0e18e636d649577a0df5ce37ee26eb74eb
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-12-16 15:10:57 -0300

    feat: create user route done

---
+++ src/interfaces/user/index.ts
@@ -0,0 +1,52 @@
+import { IAddressRequest } from "../address"
+// import { IComments } from "../comments"
+
+export interface IUser {
+  id: string
+  name: string
+  birthDate: Date
+  image: string
+  email: string
+  phone: string
+  password: string
+  cpf: string
+  isActive: boolean
+  isSeller: boolean
+  bio: string
+  address: IAddressRequest
+  vehicles: []
+  //   comments: IComments[]
+}
+
+export interface IUserRequest {
+  name: string
+  email: string
+  cpf: string
+  password: string
+  isSeller: boolean
+  birthDate?: Date
+  phone?: string
+  bio?: string
+  address: IAddressRequest
+}
+
+export interface IUserLogin {
+  email: string
+  senha: string
+}
+
+export interface IUpdateUSer {
+  name?: string
+  birthDate?: Date
+  image?: string
+  email?: string
+  phone?: string
+  password?: string
+  cpf?: string
+  isActive?: boolean
+  isSeller?: boolean
+  bio?: string
+  address?: string
+  vehicles?: []
+  //   comments?: IComments[]
+}
---
+++ src/services/user/createUser.service.ts
@@ -0,0 +1,56 @@
+import { hash } from "bcryptjs"
+import AppDataSource from "../../data-source"
+import { User } from "../../entities/user.entity"
+import { AppError } from "../../errors/appError"
+import { IUserRequest } from "../../interfaces/user"
+
+export const createUserService = async ({
+  name,
+  email,
+  address,
+  cpf,
+  password,
+  isSeller,
+}: IUserRequest): Promise<User> => {
+  const userRepository = AppDataSource.getRepository(User)
+
+  const userAlreadyExists = await userRepository.findOne({
+    where: {
+      cpf,
+    },
+  })
+
+  if (userAlreadyExists) {
+    throw new AppError("Usuário já cadastrado!", 400)
+  }
+
+  const hashedPassword = await hash(password, 10)
+
+  if (isSeller) {
+    const newSeller = userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      cpf,
+      isSeller: true,
+      address,
+    })
+
+    await userRepository.save(newSeller)
+
+    return newSeller
+  }
+
+  const normalUser = userRepository.create({
+    name,
+    email,
+    password: hashedPassword,
+    cpf,
+    isSeller: false,
+    address,
+  })
+
+  await userRepository.save(normalUser)
+
+  return normalUser
+}
