8747548578d5c3204007a484951d24ea1b549b44
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2023-01-03 11:52:19 -0300

    feat: recovery password task initi

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
-import { Express } from "express";
+import { Express } from "express"
 
-import { usersRoutes } from "./users.routes";
-import { loginRoutes } from "./login.routes";
-import { vehiclesRoutes } from "./vehicles.routes";
-import { commentsRoutes } from "./comments.routes";
+import { usersRoutes } from "./users.routes"
+import { loginRoutes } from "./login.routes"
+import { vehiclesRoutes } from "./vehicles.routes"
+import { commentsRoutes } from "./comments.routes"
+import { recoveryPasswordRoutes } from "./recoveryPassword.routes"
 
 export const appRoutes = (app: Express) => {
-  app.use("/users", usersRoutes());
-  app.use("/vehicles", vehiclesRoutes());
-  app.use("/login", loginRoutes());
-  app.use("/comments", commentsRoutes());
-};
+  app.use("/users", usersRoutes())
+  app.use("/vehicles", vehiclesRoutes())
+  app.use("/login", loginRoutes())
+  app.use("/recovery-password", recoveryPasswordRoutes())
+  app.use("/comments", commentsRoutes())
+}
