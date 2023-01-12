bb206b461e5462d82e2fd6a8993934cb6087e27b
Matheus <matheus_lsilva@hotmail.com>
2023-01-12 12:30:04 -0300

    fix: retornando imagens na criacao do veiculo

--- src/controllers/vehicle/vehicles.controllers.ts
+++ src/controllers/vehicle/vehicles.controllers.ts
@@ -1,45 +1,44 @@
-import { Request, Response } from "express";
-import { IReqCreateVehicle } from "../../interfaces/vehicle";
-import { createVehicleService } from "../../services/vehicles/createVehicle";
-import { createVehicleServiceNoUser } from "../../services/vehicles/createVehicle.noUser";
-import vehicleListIdService from "../../services/vehicles/listVehicleId";
+import { Request, Response } from 'express';
+import { IReqCreateVehicle } from '../../interfaces/vehicle';
+import { createVehicleService } from '../../services/vehicles/createVehicle';
+import vehicleListIdService from '../../services/vehicles/listVehicleId';
 
-import { listVehicleService } from "../../services/vehicles/listVehicles";
+import { listVehicleService } from '../../services/vehicles/listVehicles';
+import { User } from '../../entities/user.entity';
 
-
-export const createVehicleControllerNoUser = async (
-  req: Request,
-  res: Response
-) => {
-  const data: IReqCreateVehicle = req.body;
-  const response = await createVehicleServiceNoUser(data);
-  return res.status(201).json(response);
-};
-
-// export const createVehicleControllerNoUser = async (req: Request, res: Response) => {
-//     const data: IReqCreateVehicle = req.body;
-//     const response = await createVehicleServiceNoUser(data);
-//     return res.status(201).json(response);
-// };
+interface ICreateVehicle {
+    userId: string;
+    color: string;
+    type: string;
+    price: string;
+    plate: string;
+    images: string[];
+    km: number;
+    year: string;
+    title: string;
+    description: string;
+    isActive: boolean;
+    user: User;
+}
 
 export const createVehicleController = async (req: Request, res: Response) => {
-  const data: IReqCreateVehicle = req.body;
-  const userId = req.params.userId;
-  data.userId = userId;
-  const response = await createVehicleService(data);
-  return res.status(201).json(response);
+    const data: IReqCreateVehicle = req.body;
+    const userId = req.params.userId;
+    data.userId = userId;
+    const response = await createVehicleService(data as ICreateVehicle);
+    return res.status(201).json(response);
 };
 
 export const listVehiclesController = async (req: Request, res: Response) => {
-  const response = await listVehicleService();
-  return res.status(200).json(response);
+    const response = await listVehicleService();
+    return res.status(200).json(response);
 };
 
-export const vehicleListVeicleIdController = async (
-  req: Request,
-  res: Response
+export const vehicleListVehicleIdController = async (
+    req: Request,
+    res: Response
 ) => {
-  const vehicleId: string = req.params.userId;
-  const vehicle = await vehicleListIdService(vehicleId);
-  return res.status(200).json(vehicle);
+    const vehicleId: string = req.params.userId;
+    const vehicle = await vehicleListIdService(vehicleId);
+    return res.status(200).json(vehicle);
 };
--- src/interfaces/vehicle/index.ts
+++ src/interfaces/vehicle/index.ts
@@ -1,13 +1,13 @@
 export interface IReqCreateVehicle {
-    "color": string,
-    "type": string,
-    "price": string,
-    "plate": string,
-    "image": string,
-    "km": number,
-    "year": string,
-    "title": string,
-    "description": string,
-    "isActive": boolean,
-    "userId"?: string
+    color: string;
+    type: string;
+    price: string;
+    plate: string;
+    images: string[];
+    km: number;
+    year: string;
+    title: string;
+    description: string;
+    isActive: boolean;
+    userId?: string;
 }
